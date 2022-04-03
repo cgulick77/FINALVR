1b0accf1ead9ade50516520576bea948278ecf52
Christian <cgulick@student.fitchburgstate.edu>
2022-04-03 19:58:44 -0400

    Direct Interactor
    changed interactor to direct interactor, now have one hand climbing

--- Assets/Scripts/ClimableSurface.cs
+++ Assets/Scripts/ClimableSurface.cs
@@ -3,39 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
+
 public class ClimableSurface : XRGrabInteractable
 {
     private ControllerVelocity controllerVelocity = null;
     private Climber climber;
     public bool entered;
+
+    //  void Start() {
+    //     XRBaseInteractor interactor = selectingInteractor;
+
+    //     IXRSelectInteractor newInteractor = firstInteractorSelecting;
+
+    //     List<IXRSelectInteractor> moreInteractors = interactorsSelecting;
+    // }
         protected override void Awake()
     {
         base.Awake();
         climber = FindObjectOfType<Climber>();
 
+      
+        
     }
     
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
         controllerVelocity = args.interactor.GetComponent<ControllerVelocity>();
-
-        
-       
-        //climber.hand = GetComponent<XRBaseInteractor>();
+        climber.hand = args.interactor.GetComponent<XRController>();
+        //XRController controller = args.interactorObject;
+      
 
     }
 
     protected override void OnSelectExited(SelectExitEventArgs args)
     {
         base.OnSelectExited(args);
-        //entered = false;
-
-        
-
+        controllerVelocity = null;
+         
     }
-
-
+    
     public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
     {
         base.ProcessInteractable(updatePhase);
@@ -44,6 +51,7 @@ public class ClimableSurface : XRGrabInteractable
         {
             if(updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
             {
+                
                 climber.Climb();
             }
 
--- Assets/Scripts/Climber.cs
+++ Assets/Scripts/Climber.cs
@@ -1,55 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit;
-using UnityEngine.XR;
-using UnityEngine.InputSystem;
-
-public class Climber : MonoBehaviour
-{
-    public CharacterController character;
-    public  XRController hand;
-    private ControllerVelocity controllerVelocity;
-    public LayerMask groundLayer;
-    private ClimableSurface climableSurface;
-    public float gravity = -9.81f;
-    private float fallingSpeed;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        controllerVelocity = FindObjectOfType<ControllerVelocity>();
-        climableSurface = FindObjectOfType<ClimableSurface>();
-    }
-
-    // Update is called once per frame
-     void FixedUpdate() {
-        
-      bool isGrounded = CheckIfGrounded();
-        if (isGrounded)
-        {
-             fallingSpeed = 0;
-        }
-        else {
-        fallingSpeed += gravity * Time.fixedDeltaTime;
-        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
-        }
-    }
-
-    public void Climb()
-    {
-        Vector3 velocity = controllerVelocity ? controllerVelocity.velocity : Vector3.zero;
-        character.Move(transform.rotation * -velocity * Time.deltaTime);
-    }   
-
-    bool CheckIfGrounded()
-    {
-        //tells us if on ground
-        Vector3 rayStart = transform.TransformPoint(character.center);
-        float rayLegth = character.center.y + 0.01f;
-        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLegth, groundLayer);
-        return hasHit;
-    
-    }
-
-}
+using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+using UnityEngine.XR.Interaction.Toolkit; 
+using UnityEngine.XR; 
+using UnityEngine.InputSystem; 
+ 
+public class Climber : MonoBehaviour 
+{ 
+    public CharacterController character; 
+    public  XRController hand; 
+    private ControllerVelocity controllerVelocity; 
+    private ClimableSurface climableSurface; 
+    public float climbSpeed; 
+    public List<XRController> climbingHands = new List<XRController>(); 
+     
+    // Start is called before the first frame update 
+    void Start() 
+    { 
+        controllerVelocity = FindObjectOfType<ControllerVelocity>(); 
+        climableSurface = FindObjectOfType<ClimableSurface>(); 
+    } 
+ 
+    // Update is called once per frame 
+     void FixedUpdate() { 
+ 
+         if (hand) 
+         { 
+             Climb(); 
+         } 
+         
+       
+     } 
+ 
+ 
+    public void Climb() 
+    { 
+        //Gets controllers velocity 
+        Vector3 velocity = controllerVelocity ? controllerVelocity.velocity : Vector3.zero; 
+        character.Move(transform.rotation * -velocity * Time.deltaTime * climbSpeed); 
+    }    
+ 
+     
+ 
+} 
